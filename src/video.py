baad1c24753a8b64703a09ba4c3c9aaa83eb48f1
nikolay <nikkot1981@gmail.com>
2023-08-22 20:15:32 +0400

    дз 16.1 задание 2

--- src/video.py
+++ src/video.py
@@ -5,11 +5,16 @@ class Video:
     def __init__(self, video_id):
         self.video_id = video_id
         video_response = build('youtube', 'v3', developerKey=os.getenv('API_KEY')).videos().list(part='snippet,statistics,contentDetails,topicDetails', id=self.video_id).execute()
-        self.video_title: str = video_response['items'][0]['snippet']['title']
-        self.url = f'https://www.youtube.com/watch?v={self.video_id}'
-        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
-
+        if len(video_response['items']) != 0:
+            self.title: str = video_response['items'][0]['snippet']['title']
+            self.url = f'https://www.youtube.com/watch?v={self.video_id}'
+            self.view_count: int = video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+        else:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
     def __str__(self):
         return f'{self.video_title}'
 
@@ -17,3 +22,4 @@ class PLVideo(Video):
     def __init__(self, video_id, playlist_id):
         super().__init__(video_id)
         self.playlist_id = playlist_id
+
